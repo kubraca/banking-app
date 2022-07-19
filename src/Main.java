77b6bf2e643dc4977af1c9f869de6be2d8166bbb
Murat Kuyumcu <murat.kuyumcu@experilabs.com>
2022-07-19 17:59:38 +0300

    murat

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hallo from ssdcs");
         System.out.println("Hello world!");
+        System.out.println("Hello frmmmmmmmmmmm!");
     }
 }
